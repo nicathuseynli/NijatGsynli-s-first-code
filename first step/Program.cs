557e18e14d70d7cd79c43d485c4cd468057a69c0
nicathuseynli <nijatkhh@code.edu.az>
2022-07-31 09:39:19 +0400

    first step

---
+++ first step/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Amateur Web Developer");
+string name = "Nidjat";
+int age = 18;
+char sex = 'M';
+string work = "Student of Ondokuz Mayis University";
+int a = 28;
+int b = 13;
+int c = 9;
+
+Console.WriteLine(a + b + c);
+Console.WriteLine(a == b);
+Console.WriteLine(a >= b);
+Console.WriteLine(a <= c);
+Console.WriteLine(a / b);
+Console.WriteLine(b % c);
+
